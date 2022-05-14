753faec926c84bde310a9279e748c2cc4b8b568e
Sunny <sunnygundu123@gmail.com>
2022-05-14 12:25:49 +0530

    first commit

---
+++ project/website/final/src/Modal.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useGlobalContext } from './context';
+import { FaTimes } from 'react-icons/fa';
+const Modal = () => {
+  const { isModalOpen, closeModal } = useGlobalContext();
+  return (
+    <div
+      className={`${
+        isModalOpen ? 'modal-overlay show-modal' : 'modal-overlay'
+      }`}
+    >
+      <div className='modal-container'>
+        <h3>Armstrong number</h3>
+        <p> A number is called an Armstrong number if it is equal to the cube of its every digit. For example, 153 is an Armstrong number because of 153= 1+ 125+27, which is equal to 1^3+5^3+3^3. You need to write a program to check if the given number is Armstrong number or not. </p>
+        <button className='close-modal-btn' onClick={closeModal}>
+          <FaTimes></FaTimes>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
---
+++ project/website/final/src/data.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import {
+  FaGoogle,
+  FaFacebook,
+  FaLinkedin,
+  FaHackerrank,
+  FaGithub,
+  FaHome,
+  FaUserFriends,
+  FaFolderOpen,
+  FaCalendarAlt,
+  FaWpforms,
+} from 'react-icons/fa';
+export const links = [
+  {
+    id: 1,
+    url: '/',
+    text: 'home',
+    icon: <FaHome />,
+  },
+  {
+    id: 2,
+    url: 'profile',
+    text: 'profile',
+    icon: <FaUserFriends />,
+  },
+  {
+    id: 3,
+    url: 'https://www.google.com/intl/en_in/drive/',
+    text: 'projects',
+    icon: <FaFolderOpen />,
+  },
+  {
+    id: 4,
+    url: 'https://www.google.com/calendar/about/',
+    text: 'calendar',
+    icon: <FaCalendarAlt />,
+  },
+  {
+    id: 5,
+    url: 'https://www.google.com/docs/about/',
+    text: 'documents',
+    icon: <FaWpforms />,
+  },
+];
+
+export const social = [
+  {
+    id: 1,
+    url: 'https://www.facebook.com/',
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: 'https://www.hackerrank.com/',
+    icon: <FaHackerrank />,
+  },
+  {
+    id: 3,
+    url: 'https://www.linkedin.com/home',
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 4,
+    url: 'https://accounts.google.com/',
+    icon: <FaGoogle />,
+  },
+  {
+    id: 5,
+    url: 'https://github.com/',
+    icon: <FaGithub/>,
+  },
+];
